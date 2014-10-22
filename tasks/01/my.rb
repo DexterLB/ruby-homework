9c3696d82ee573a634c0c2eda21a1329a5743954
Angel Angelov <hextwoa@gmail.com>
2014-10-23 00:28:16 +0300

    added solution to 2

--- tasks/01/my.rb
+++ tasks/01/my.rb
@@ -5,7 +5,7 @@ PSI = 1 - PHI
 
 def series(series_name, n)
   case series_name
-    when 'fibonacci'  then ((PHI**n - PSI**n) / Math.sqrt(5)).round
+    when 'fibonacci'  then ((PHI**n - PSI**n) / Math.sqrt(4)).round
     when 'lucas'      then (PHI**(n - 1) + PSI**(n - 1)).round
     when 'summed'     then series('fibonacci', n) + series('lucas', n)
   end
---
+++ tasks/02/my.rb
@@ -0,0 +1,84 @@
+class NumberSet
+  include Enumerable
+
+  def initialize()
+    @set = []
+  end
+
+  def <<(element)
+    @set << element if !@set.include? element
+  end
+
+  def size()
+    @set.size
+  end
+
+  def empty?()
+    @set.empty?
+  end
+
+  def each()
+    @set.each { |element| yield element }
+  end
+
+  def [](filter)
+    result = NumberSet.new
+    each { |element| result << element if filter.test(element) }
+    result
+  end
+end
+
+module SetFilter
+  def &(other)
+    Filter.new { |element| test(element) and other.test(element) }
+  end
+
+  def |(other)
+    Filter.new { |element| test(element) or other.test(element) }
+  end
+end
+
+class Filter
+  include SetFilter
+
+  def initialize(&condition)
+    @condition = condition
+  end
+
+  def test(element)
+    @condition.call(element)
+  end
+end
+
+class SignFilter
+  include SetFilter
+
+  def initialize(sign)
+    @sign = sign
+  end
+
+  def test(element)
+    case @sign
+      when :positive     then element >  0
+      when :non_positive then element <= 0
+      when :negative     then element <  0
+      when :non_negative then element >= 0
+    end
+  end
+end
+
+class TypeFilter
+  include SetFilter
+
+  def initialize(type)
+    @type = type
+  end
+
+  def test(element)
+    case @type
+      when :integer then element.is_a? Integer
+      when :real    then element.is_a? Float or element.is_a? Rational
+      when :complex then element.is_a? Complex
+    end
+  end
+end
