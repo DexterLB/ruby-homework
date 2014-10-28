d220e6dd0844b8113933ce3d6319ae4257fd59c6
Angel Angelov <hextwoa@gmail.com>
2014-10-28 11:30:45 +0200

    fixed tabs

--- challenges/03/my.rb
+++ challenges/03/my.rb
@@ -1,14 +1,14 @@
 class String
-    def word_wrap(chars_per_row)
-        row = ''
-        split.each_with_object([]) do |word, wrapped|
-            if (row + word).length + 1 > chars_per_row
-                wrapped << row unless row.empty?
-                row = word
-            else
-                row += ' ' unless row.empty?
-                row += word
-            end
-        end + (row.empty? ? [] : [row])
-    end
+  def word_wrap(chars_per_row)
+    row = ''
+    split.each_with_object([]) do |word, wrapped|
+      if (row + word).length + 1 > chars_per_row
+        wrapped << row unless row.empty?
+        row = word
+      else
+        row += ' ' unless row.empty?
+        row += word
+      end
+    end + (row.empty? ? [] : [row])
+  end
 end
